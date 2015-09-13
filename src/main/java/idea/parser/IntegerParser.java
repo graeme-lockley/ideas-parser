8232b4c58e3e84f2dac75ef92722e964ba78187d
Graeme Lockley <graemel@no9.co.za>
2015-09-13 20:33:56 +0200

    Simplified the Integer Parser by assuming a type structure for now.

--- src/main/java/idea/parser/IntegerParser.java
+++ src/main/java/idea/parser/IntegerParser.java
@@ -1,28 +1,11 @@
 package idea.parser;
 
-public class IntegerParser extends SequenceParser<Lexer, Either<String, Tuple<Lexer, Integer>>, Either<String, Tuple<Lexer, Integer>>, Lexer> {
+public class IntegerParser extends SequenceParser<Integer, Integer, Integer> {
     /*
         Minus?  Digit+
      */
 
     public IntegerParser() {
-        super(new Minus(), new Digit());
-    }
-
-    @Override
-    public Either<String, Tuple<Lexer, Integer>> parse(Lexer input) {
-        final Either<String, Tuple<Lexer, Integer>> firstResponse = first.parse(input);
-
-        if (firstResponse.isLeft()) {
-            return firstResponse;
-        } else {
-            final Either<String, Tuple<Lexer, Integer>> secondResponse = second.parse(firstResponse.right().v1);
-
-            if (secondResponse.isLeft()) {
-                return secondResponse;
-            } else {
-                return Either.right(Tuple.from(secondResponse.right().v1, firstResponse.right().v2 * secondResponse.right().v2));
-            }
-        }
+        super(new Minus(), new Digit(), (Integer v1, Integer v2) -> v1 * v2);
     }
 }
--- src/main/java/idea/parser/SequenceParser.java
+++ src/main/java/idea/parser/SequenceParser.java
@@ -1,16 +1,37 @@
 package idea.parser;
 
-public abstract class SequenceParser<I, O, A, B> extends Thing<I, O> {
-    protected final Thing<I, A> first;
-    protected final Thing<B, O> second;
+import java.util.function.BiFunction;
 
-    public SequenceParser(Thing<I, A> first, Thing<B, O> second) {
+public abstract class SequenceParser<A, B, C> extends Thing<Lexer, Either<String, Tuple<Lexer, C>>> {
+    protected final Thing<Lexer, Either<String, Tuple<Lexer, A>>> first;
+    protected final Thing<Lexer, Either<String, Tuple<Lexer, B>>> second;
+    private BiFunction<A, B, C> mergeFunction;
+
+    public SequenceParser(Thing<Lexer, Either<String, Tuple<Lexer, A>>> first, Thing<Lexer, Either<String, Tuple<Lexer, B>>> second, BiFunction<A, B, C> mergeFunction) {
         this.first = first;
         this.second = second;
+        this.mergeFunction = mergeFunction;
     }
 
     @Override
-    public boolean canParse(I input) {
-        return first.canParse(input);
+    public boolean canParse(Lexer input) {
+        return false;
+    }
+
+    @Override
+    public Either<String, Tuple<Lexer, C>> parse(Lexer input) {
+        final Either<String, Tuple<Lexer, A>> firstResponse = first.parse(input);
+
+        if (firstResponse.isLeft()) {
+            return Either.left(firstResponse.left());
+        } else {
+            final Either<String, Tuple<Lexer, B>> secondResponse = second.parse(firstResponse.right().v1);
+
+            if (secondResponse.isLeft()) {
+                return Either.left(secondResponse.left());
+            } else {
+                return Either.right(Tuple.from(secondResponse.right().v1, mergeFunction.apply(firstResponse.right().v2, secondResponse.right().v2)));
+            }
+        }
     }
 }
